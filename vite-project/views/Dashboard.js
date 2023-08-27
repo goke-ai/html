0b94c198c3b0b3bbf63f6cd23a684836ba44d2df
goke Oladokun <gokeladokun@gmail.com>
2023-08-27 20:06:32 +0100

    Fix vite-project css

--- vite-project/views/Dashboard.js
+++ vite-project/views/Dashboard.js
@@ -8,10 +8,10 @@ export default class extends AbstractView {
 
     async getHtml() {
         return `
-    <div class="wrapper flow">
-        <h1>Home</h1>
-        <h2>Lorem ipsum dolor sit amet consectetur.</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim est nesciunt, cum fugit veniam
+        <div class="wrapper flow">
+          <h1>Home</h1>
+          <h2>Lorem ipsum dolor sit amet consectetur.</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim est nesciunt, cum fugit veniam
             animi in
             modi perspiciatis? Aliquid officia eaque aliquam eveniet eum illum in nihil similique amet at.
             Et blanditiis eveniet neque quos quis odio ipsam suscipit, autem illo necessitatibus laudantium,
@@ -20,34 +20,34 @@ export default class extends AbstractView {
             Quis ex est officiis porro laboriosam voluptatem numquam incidunt cumque. Doloremque aliquid
             odio. </p>
 
-        <div class="card">
+          <div class="card">
             <img src="images/background.jpg" alt="">
             <div class="card-body">
-                at maiores fugit quam eum illo obcaecati temporibus laudantium earum dolore ab quis? Impedit
-                illum praesentium eligendi?
+              at maiores fugit quam eum illo obcaecati temporibus laudantium earum dolore ab quis? Impedit
+              illum praesentium eligendi?
             </div>
-        </div>
-        <div class="card-secondary">
+          </div>
+          <div class="card-secondary">
             <img src="images/planets-collide-wide-wallpaper-22488.jpg" alt="">
             <div class="card-body">
-                Quis saepe error consequatur dicta distinctio minima asperiores sunt? Consequuntur incidunt
-                adipisci eius numquam omnis corporis qui ab! Magni incidunt, eius dignissimos placeat
-                numquam ut cum reiciendis architecto minus soluta.
+              Quis saepe error consequatur dicta distinctio minima asperiores sunt? Consequuntur incidunt
+              adipisci eius numquam omnis corporis qui ab! Magni incidunt, eius dignissimos placeat
+              numquam ut cum reiciendis architecto minus soluta.
             </div>
-        </div>
-        <div class="card">
+          </div>
+          <div class="card">
 
             <div class="card-body">
-                Unde possimus commodi impedit rem minus aut pariatur libero magni illum consequuntur id
-                laborum
-                aliquam, rerum accusantium fuga voluptate ipsa assumenda dolores ab maxime, et accusamus
-                eius?
-                Tempore, quod voluptatum!
+              Unde possimus commodi impedit rem minus aut pariatur libero magni illum consequuntur id
+              laborum
+              aliquam, rerum accusantium fuga voluptate ipsa assumenda dolores ab maxime, et accusamus
+              eius?
+              Tempore, quod voluptatum!
             </div>
-        </div>
+          </div>
 
-        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim est nesciunt, cum fugit veniam
+          <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim est nesciunt, cum fugit veniam
             animi in
             modi perspiciatis? Aliquid officia eaque aliquam eveniet eum illum in nihil similique amet at.
             Et blanditiis eveniet neque quos quis odio ipsam suscipit, autem illo necessitatibus laudantium,
@@ -66,8 +66,8 @@ export default class extends AbstractView {
             aliquam, rerum accusantium fuga voluptate ipsa assumenda dolores ab maxime, et accusamus eius?
             Tempore, quod voluptatum!</p>
 
-        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, nulla.</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim est nesciunt, cum fugit veniam
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, nulla.</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim est nesciunt, cum fugit veniam
             animi in
             modi perspiciatis? Aliquid officia eaque aliquam eveniet eum illum in nihil similique amet at.
             Et blanditiis eveniet neque quos quis odio ipsam suscipit, autem illo necessitatibus laudantium,
@@ -86,11 +86,11 @@ export default class extends AbstractView {
             aliquam, rerum accusantium fuga voluptate ipsa assumenda dolores ab maxime, et accusamus eius?
             Tempore, quod voluptatum!</p>
 
-        <h1>Lorem ipsum dolor sit.</h1>
-        <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h2>
-        <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa dolores commodi corporis
+          <h1>Lorem ipsum dolor sit.</h1>
+          <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h2>
+          <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa dolores commodi corporis
             incidunt veritatis suscipit.</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim est nesciunt, cum fugit veniam
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim est nesciunt, cum fugit veniam
             animi in
             modi perspiciatis? Aliquid officia eaque aliquam eveniet eum illum in nihil similique amet at.
             Et blanditiis eveniet neque quos quis odio ipsam suscipit, autem illo necessitatibus laudantium,
@@ -109,8 +109,7 @@ export default class extends AbstractView {
             aliquam, rerum accusantium fuga voluptate ipsa assumenda dolores ab maxime, et accusamus eius?
             Tempore, quod voluptatum!</p>
 
-    </div>
-
-        `;
+        </div>
+      `;
     }
 }
--- vite-project/views/_Navbar.js
+++ vite-project/views/_Navbar.js
@@ -8,30 +8,31 @@ export default class extends _AbstractPartialView {
     async getHtml() {
         return `
         <nav class="primary-navigation flex">
-            <div class="logo"><span class="icon">LOGO</span></div>
+        <div class="logo"><span class="icon">LOGO</span></div>
 
-            <input type="checkbox" class="navbar-controller navbar-controller-checkbox" name="navbar-controller" id="navbar-controller">
-            <label class="navbar-controller navbar-controller-label" for="navbar-controller"><span class="icon"></span></label>
+        <input type="checkbox" class="navbar-controller" name="navbar-controller" id="navbar-controller">
+        <label class="navbar-controller-label" for="navbar-controller"><span class="icon">X</span></label>
 
-            <ul class="primary-nav-list flex">
-                <li><a class="active" data-link href="/">Home <span>🏡</span></a></li>
-                <li><a data-link href="/contact">Contact</a></li>
-                <li class="dropdown"><a href="#">Account</a>
-                    <ul class="dropdown-list">
-                        <li><a data-link href="/login">Login</a></li>
-                        <li><a data-link href="/register">Register</a></li>
-                        <li><a data-link href="/login">Sub Menu3 3</a></li>
-                    </ul>
-                </li>
-                <li class="dropdown"><a href="#">Componets Menu Four</a>
-                    <ul class="dropdown-list pop-down-left">
-                        <li><a data-link href="/contact">Forms</a></li>
-                        <li><a data-link href="/components">Components</a></li>
-                    </ul>
-                </li>
+        <ul class="primary-nav-list flex">
+          <li><a class="active" href="#">Home</a></li>
+          <li><a href="contact.html">Contact</a></li>
+          <li class="dropdown"><a href="#">Account</a>
+            <ul class="dropdown-list">
+              <li><a href="#">Login</a></li>
+              <li><a href="register.html">Register</a></li>
+              <li><a href="#">Sub Menu3 3</a></li>
             </ul>
-        </nav>
-        `
+          </li>
+          <li class="dropdown"><a href="#">Components Menu Four</a>
+            <ul class="dropdown-list pop-down-left">
+              <li><a href="contact.html">Forms</a></li>
+              <li><a href="components.html">Components</a></li>
+              <li><a href="#">Sub Menu3 Menu X3</a></li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    `
     }
 }
 
